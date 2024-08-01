b352e50837e0f36739c78996dd35de1385db2c1e
AngRod <sthefanyangeles00@gmail.com>
2024-08-01 07:21:14 -0400

    V.1 Finished (Actualization css and update menu of animes)

--- app.py
+++ app.py
@@ -10,32 +10,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Fetches all the anime from the database.
+    
+    #save_image_to_db('Blue-Lock','static/images/blue-lock-picture.jpg', 24, 'Action, Shounen, Sports', 'Manga', 'Ongoing', '2021', 8.5, 'The project is ultimate goal is to turn one of the selected players into the star striker for the Japanese national team. To find the best participant, each diamond in the rough must compete against others through a series of solo and team competitions to rise to the top. Putting aside his ethical objections to the project, Isagi feels compelled to fight his way to the top, even if it means ruthlessly crushing the dreams of 299 aspiring young strikers.')
+    animes = fetch_anime_images()
+    #image_base64 = get_image_from_db(int(animes[0][0]))
+    # Renders the index.html template with the anime and image.
+    return render_template('index.html', animes=animes)
+
+
+def fetch_anime_images():
     conn = sqlite3.connect('anime.db', check_same_thread=False)
     db = conn.cursor()
     animes = db.execute('SELECT * FROM anime').fetchall()
     if not animes:
         conn.close()
-        return 'No anime found in the database.'
-    # Fetches the image from the database.
-    image_base64 = get_image_from_db(int(animes[0][0]))
-    # Renders the index.html template with the anime and image.
-    return render_template('index.html', animes=animes, image_base64=image_base64)
+        return []
+    
+    updated_animes = []
+    for anime in animes:
+        anime = list(anime)
+        anime[2] = base64.b64encode(anime[2]).decode('utf-8') 
+        updated_animes.append(anime)
+    
+    conn.close()
+    return updated_animes
 
+# Function to add anime to the database.
 def save_image_to_db(title, image, episodes, genre, type, 
-status, start_date, end_date, rating, members):
+status, date, rating, description):
     
     conn = sqlite3.connect('anime.db', check_same_thread=False)
     db = conn.cursor()
+    # Opens the image file.
     with open(image, 'rb') as file:
         blob_data = file.read()
-
-    db.execute('INSERT INTO anime (title, image, episodes, genre, type, status, start_date, end_date, rating, members) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-              (title, blob_data, episodes, genre, type, status, start_date, end_date, rating, members))
+    # Inserts the anime into the database.
+    db.execute('INSERT INTO anime (title, image, episodes, genre, type, status, date, rating, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+              (title, blob_data, episodes, genre, type, status, date, rating, description))
     
     conn.commit()
     conn.close()
 
+# Function to get the image from the database.
 def get_image_from_db(anime_id):
 
     conn = sqlite3.connect('anime.db', check_same_thread=False)
